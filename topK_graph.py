9f8704a2875ca713b5c769944706545eaaede63b
oshita30 <84271127+oshita30@users.noreply.github.com>
2021-07-21 12:43:41 +0530

    Update topK_graph.py

--- topK_graph.py
+++ topK_graph.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     
     dict_k={}
     
-    for k in range(30,150,10):
+    for k in range(100,500,10):
         blue_scores = load_kNN_model(org_diff_code=org_diff_data, tf_diff_code=tf_diff_data, ref_msg=ref_data, topK=k)
         dict_k[k] = sum(blue_scores) / len(blue_scores) * 100
         print('Average of blue scores for k=',k,': ', sum(blue_scores) / len(blue_scores) * 100)
